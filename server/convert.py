b23ba23be8fddbcc3727b6ab39eae8911477d526
Shakeel Khalid <191370070@gift.edu.pk>
2023-04-10 17:43:32 +0500

    using transformer api for whisper base model and also splitting the comming media files to 30 seconds files and get transcript. adding NLP to get glossary

--- server/convert.py
+++ server/convert.py
@@ -1,18 +1,55 @@
-import whisper
 import os
 import re
+import requests
+import subprocess
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 
-type = 'small'
-model = whisper.load_model(type)
+from glossary import clean_sentence
+
+API_URL = "https://api-inference.huggingface.co/models/openai/whisper-base"
+TOKEN = "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"
+
+headers = {"Authorization": f"Bearer {TOKEN}"}
+
+def get_duration(filename):
+    command = ['ffprobe', '-i', filename, '-show_entries', 'format=duration', '-v', 'quiet', '-of', 'csv=%s' % ("p=0")]
+    output = subprocess.check_output(command, shell=True)
+    return float(output)
+
+def split_file_subparts(filename, subpart_duration, ext):
+    # Run the ffmpeg command to split the video into subparts
+    command = ['ffmpeg', '-i', filename, '-c', 'copy', '-map', '0', '-segment_time',
+               str(subpart_duration), '-f', 'segment', f'temp/media_%03d.{ext}']
+    subprocess.call(command)
 
 def audio_to_text(filename):
+    transcript = ""
     print("Converting audio to text... Please wait.")
-    result = model.transcribe(filename, fp16=False)
-    os.remove(filename)
-    text = result["text"]
-    return text
+
+    subpart_duration = 30
+    ext = filename.split('.')[-1]
+
+    duration = get_duration(filename)
+    split_file_subparts(filename, subpart_duration, ext)
+
+    subparts = [f'temp/media_{i:03d}.{ext}' for i in range(int(duration / subpart_duration) + 1)]
+
+    for subpart in subparts:
+        if get_duration(subpart) < 2:
+            continue
+        with open(subpart, "rb") as f:
+            data = f.read()
+        response = requests.post(API_URL, headers=headers, data=data)
+
+        output = response.json()
+        transcript = transcript + output['text']
+
+    # os.remove(filename)
+    for subpart in subparts:
+        os.remove(subpart)
+
+    return transcript
 
 def transcript_video(url):
     # get id of youtube video from url
@@ -32,4 +69,13 @@ def transcript_video(url):
 
 # for testing purpose
 if __name__ == "__main__":
-    print(transcript_video('https://youtu.be/lMvFWKHhVZ0'))
+    # print(transcript_video('https://youtu.be/lMvFWKHhVZ0'))
+    filename = "vocal-spoken-the-realm-female-speech_75bpm_C_major.wav"
+
+    output = audio_to_text(filename)
+
+    print(output)
+
+    print("=" * 50)
+    print("Output after cleaning:")
+    print(clean_sentence(output))
