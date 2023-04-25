a2996a702bbd12760a3a8b3f10b7c70290a471e7
Shakeel Khalid <191370070@gift.edu.pk>
2023-04-25 20:57:40 +0500

    improve code

--- server/main.py
+++ server/main.py
@@ -60,4 +60,4 @@ async def get_glossary(text: Text):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
